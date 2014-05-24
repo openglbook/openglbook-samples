86ee60c4b45ba5b3f74496ad09ca50e947566c4e
Eddy Luten <eddyluten@gmail.com>
2014-05-24 02:36:52 -0700

    openglbook.com issue #26: fix ExitOnGLError impl See https://github.com/openglbook/openglbook.com/issues/26 for details.

--- chapter-4/Utils.c
+++ chapter-4/Utils.c
@@ -131,17 +131,7 @@ void ExitOnGLError(const char* error_message)
 
   if (ErrorValue != GL_NO_ERROR)
   {
-    const char* APPEND_DETAIL_STRING = ": %s\n";
-    const size_t APPEND_LENGTH = strlen(APPEND_DETAIL_STRING) + 1;
-    const size_t message_length = strlen(error_message);
-    char* display_message = (char*)malloc(message_length + APPEND_LENGTH);
-
-    memcpy(display_message, error_message, message_length);
-    memcpy(&display_message[message_length], APPEND_DETAIL_STRING, APPEND_LENGTH);
-
-    fprintf(stderr, display_message, gluErrorString(ErrorValue));
-
-    free(display_message);
+    fprintf(stderr, "%s: %s\n", error_message, gluErrorString(ErrorValue));
     exit(EXIT_FAILURE);
   }
 }
